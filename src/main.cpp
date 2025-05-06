b95a08deea63742b829084df88fb4997dd6e2b4d
alonsoo-rodriguez <kamran810304@gmail.com>
2025-05-06 20:06:27 +0100

    working bootloader

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
-extern "C" {
-  #include "stm32l4xx.h"  // or stm32l431xx.h if needed
+extern "C"
+{
+#include "stm32l4xx.h" // or stm32l431xx.h if needed
 }
 #include <Arduino.h>
 #include <dronecan.h>
@@ -9,6 +10,7 @@ extern "C" {
 DroneCAN dronecan;
 
 uint32_t looptime = 0;
+
 
 /*
 This function is called when we receive a CAN message, and it's accepted by the shouldAcceptTransfer function.
@@ -70,46 +72,100 @@ static bool shouldAcceptTransfer(const CanardInstance *ins,
 
 void setup()
 {
+
     SCB->VTOR = 0x0800A000;
+    RCC_OscInitTypeDef RCC_OscInitStruct = {};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+
+    /** Configure the main internal regulator output voltage
+     */
+    if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 1;
+    RCC_OscInitStruct.PLL.PLLN = 10;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    // Reinitialize SysTick to enable the delay() function
+    // Configure SysTick to generate an interrupt every 1ms (SystemCoreClock / 1000)
+    if (SysTick_Config(SystemCoreClock / 1000))
+    {
+        // If SysTick configuration fails, handle the error
+        Error_Handler();
+    }
+    NVIC_EnableIRQ(SysTick_IRQn);
+
     Serial.begin(115200);
-    Serial.println("Node Start");
 
     dronecan.init(onTransferReceived, shouldAcceptTransfer);
 
     IWatchdog.begin(2000000); // if the loop takes longer than 2 seconds, reset the system
+
+    while (true)
+    {
+        const uint32_t now = millis();
+
+        // send our battery message at 10Hz
+        if (now - looptime > 100)
+        {
+            looptime = millis();
+
+            // collect MCU core temperature data
+            int32_t vref = __LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION_12B);
+            int32_t cpu_temp = __LL_ADC_CALC_TEMPERATURE(vref, analogRead(ATEMP), LL_ADC_RESOLUTION_12B);
+
+            // construct dronecan packet
+            uavcan_equipment_power_BatteryInfo pkt{};
+            pkt.voltage = now / 10000;
+            pkt.temperature = cpu_temp;
+
+            // boilerplate to send a message
+            uint8_t buffer[UAVCAN_EQUIPMENT_POWER_BATTERYINFO_MAX_SIZE];
+            uint32_t len = uavcan_equipment_power_BatteryInfo_encode(&pkt, buffer);
+            static uint8_t transfer_id;
+            canardBroadcast(&dronecan.canard,
+                            UAVCAN_EQUIPMENT_POWER_BATTERYINFO_SIGNATURE,
+                            UAVCAN_EQUIPMENT_POWER_BATTERYINFO_ID,
+                            &transfer_id,
+                            CANARD_TRANSFER_PRIORITY_LOW,
+                            buffer,
+                            len);
+        }
+
+        dronecan.cycle();
+        IWatchdog.reload();
+    }
 }
 
 void loop()
 {
-    const uint32_t now = millis();
-
-    // send our battery message at 10Hz
-    if (now - looptime > 100)
-    {
-        looptime = millis();
-
-        // collect MCU core temperature data
-        int32_t vref = __LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION_12B);
-        int32_t cpu_temp = __LL_ADC_CALC_TEMPERATURE(vref, analogRead(ATEMP), LL_ADC_RESOLUTION_12B);
-
-        // construct dronecan packet
-        uavcan_equipment_power_BatteryInfo pkt{};
-        pkt.voltage = now / 10000;
-        pkt.temperature = cpu_temp;
-
-        // boilerplate to send a message
-        uint8_t buffer[UAVCAN_EQUIPMENT_POWER_BATTERYINFO_MAX_SIZE];
-        uint32_t len = uavcan_equipment_power_BatteryInfo_encode(&pkt, buffer);
-        static uint8_t transfer_id;
-        canardBroadcast(&dronecan.canard,
-                        UAVCAN_EQUIPMENT_POWER_BATTERYINFO_SIGNATURE,
-                        UAVCAN_EQUIPMENT_POWER_BATTERYINFO_ID,
-                        &transfer_id,
-                        CANARD_TRANSFER_PRIORITY_LOW,
-                        buffer,
-                        len);
-    }
-
-    dronecan.cycle();
-    IWatchdog.reload();
 }
